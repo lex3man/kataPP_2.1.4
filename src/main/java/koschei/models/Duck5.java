b1330ba1637315bca72e04610a2d2d3e6ec517cb
lex3man <lex3man@gmail.com>
2023-03-07 11:03:17 +0300

    koschei is dead

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class Duck5 {
 
+    @Autowired
+    private Egg6 egg;
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
